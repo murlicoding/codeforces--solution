56b7f6aeff6d297ce28ced57cb4c31365d4cfe29
murlicoding <rathi6153@gmail.com>
2026-06-06 08:32:14 +0530

    Add Way Too Long Words [A] from Codeforces

---
+++ 71/A - Way Too Long Words/solution.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int t;
+cin>>t;
+while(t--){
+string s;
+cin>>s;
+if(s.size()<=10)
+cout<<s<<'
+';
+else cout<<s[0]<<s.size()-2<<s[s.size()-1]<<'
+';
+}
+return 0;
+}
